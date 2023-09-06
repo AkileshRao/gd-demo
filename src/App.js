5e0c2d19fa897ef1cdadb270602033b7519bbf56
AkileshRao <akileshrao19@gmail.com>
2023-09-06 07:03:28 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { redirect } from 'react-router-dom'
 
 function App() {
+  const [repos, setRepos] = useState([])
+
+  const fetchRepos = () => {
+    fetch("https://api.github.com/orgs/godaddy/repos")
+      .then(response => response.json())
+      .then(reposResponse => setRepos(reposResponse))
+      .catch(err => console.log(err))
+  }
+
+  const handleClick = (id) => {
+    const selectedRepo = repos.find(repo => repo.id == id);
+    console.log(selectedRepo)
+    return redirect(`/repos/${selectedRepo.id}`);
+  }
+
+  useEffect(() => {
+    fetchRepos()
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-4">
+      <h1 className='text-3xl font-bold mb-4'>Github repos</h1>
+      <div className='grid grid-cols-3 gap-2'>
+        {
+          repos.map(repo => {
+            return <button key={repo.id} className='p-4 bg-cyan-300' onClick={e => handleClick(repo.id)}>
+              <p className='text-xl' >{repo.name}</p>
+            </button>
+          })
+        }
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Repo from './components/Repo';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "repos/:repoId",
+        element: <Repo />
+      }
+    ]
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
